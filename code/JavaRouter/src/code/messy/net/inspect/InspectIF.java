8199325f9ea207c3fd5d015ba39f5d052b5d1d83
alanlei@gmail.com <alanlei@gmail.com@9f69a30e-97a3-18c6-124c-73e18a0de0eb>
2014-03-25 05:55:09 +0000

    Remove unused

--- code/JavaRouter/src/code/messy/net/inspect/InspectIF.java
+++
@@ -1,7 +0,0 @@
-package code.messy.net.inspect;
-
-import java.nio.ByteBuffer;
-
-public interface InspectIF {
-	public void inspect(ByteBuffer bb);
-}
--- code/scripts/inspect.py
+++ code/scripts/inspect.py
@@ -1,15 +1,16 @@
 from code.messy.net.ethernet.bridge import Bridge
-from code.messy import Handler
+from code.messy import Receiver
 
-class Inspector(Handler):
+class Inspector(Receiver):
     def __init__(self, bypass):
         self.bypass = bypass
 
-    def handle(self, packet):
-        print "Packet len=" + str(packet.getByteBuffer().limit())
-        self.bypass.handle(packet)
+    def receive(self, packet):
+        print "Inspect: " + packet.toString()
+        self.bypass.receive(packet)
 
 bridge = Bridge("MyBridge", [interface['eth1'], interface['eth2']]);
+
 inspector = Inspector(bridge)
 
 interface['eth1'].register(inspector)

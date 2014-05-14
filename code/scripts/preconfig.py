951c4d692e4245e52fbaca2db10e5166b8567513
alanlei@gmail.com <alanlei@gmail.com@9f69a30e-97a3-18c6-124c-73e18a0de0eb>
2014-05-14 07:19:58 +0000

    Update script names

--- code/scripts/preconfig.py
+++ code/scripts/preconfig.py
@@ -1,8 +1,5 @@
 from code.messy.net.ethernet import EthernetPort
-#from code.messy.net import RawSocket
 
 interface = {}
 interface['eth1'] = EthernetPort('eth1')
 interface['eth2'] = EthernetPort('eth2')
-#interface['eth1'] = RawSocket("eth1")
-#interface['eth2'] = RawSocket("eth2")

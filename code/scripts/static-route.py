61ab4dafee64ce4495c1459e4199c904d0c89e19
alanlei@gmail.com <alanlei@gmail.com@9f69a30e-97a3-18c6-124c-73e18a0de0eb>
2014-05-19 05:11:52 +0000

    Add static-route sample

---
+++ code/scripts/static-route.py
@@ -0,0 +1,29 @@
+from code.messy.net.ethernet import Ethertype, ArpHandler
+from code.messy.net.ip import IpProtocolHandler
+from code.messy.net.ip.udp import UdpHandler
+from code.messy.net.ip.dhcp import DhcpHandler
+from code.messy.net.ip.route import LocalSubnet
+from java.net import InetAddress
+
+udp = UdpHandler()
+protocol = IpProtocolHandler()
+protocol.register(IpPacket.Protocol.UDP, udp)
+
+eth1ip = EthernetIpSupport(interface['eth1'])
+eth2ip = EthernetIpSupport(interface['eth2'])
+
+eth1ip.register(protocol);
+eth2ip.register(protocol);
+
+# arp = ArpHandler()
+
+#address = InetAddress.getByName('10.0.0.2')
+#LocalSubnet.create(address, 24, interface['eth1'])
+#address = InetAddress.getByName('10.1.0.2');
+#LocalSubnet.create(address, 24, interface['eth2'])
+
+#interface['eth1'].register(Ethertype.ARP, arp)
+#interface['eth2'].register(Ethertype.ARP, arp)
+
+interface['eth1'].start()
+interface['eth2'].start()

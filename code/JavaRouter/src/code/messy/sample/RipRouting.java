d7b7075dbbfc875384073c67f1753b09ea105e08
alanlei@gmail.com <alanlei@gmail.com@9f69a30e-97a3-18c6-124c-73e18a0de0eb>
2014-08-04 06:25:40 +0000

    Updated Rip

--- code/JavaRouter/src/code/messy/sample/RipRouting.java
+++ code/JavaRouter/src/code/messy/sample/RipRouting.java
@@ -26,12 +26,8 @@ public class RipRouting {
      * Syntax: <portname> <ip> <prefix>
      * e.g: java RipRouting eth1 10.0.0.2 24 eth2 11.0.0.2 24
      * 
-     * Routing test
+     * This would be pure static routing if RipProcessor is removed.
      * 
-     * TODO need to re-org ip address, network address, prefix, mask, port/network 
-     * 
-     * @param args
-     * @throws Exception 
      */
     public static void main(String[] args) throws Exception {
         RouteHandler route = new RouteHandler();
@@ -77,53 +73,6 @@ public class RipRouting {
         }
         
         rip.start();
-        
-        
-        
-        /*
-        List<EthernetIpSupport> ports = new ArrayList<EthernetIpSupport>();
-        
-        
-        EthernetIpSupport p = new EthernetIpSupport(new EthernetPort(args[0]));
-        InetAddress address = InetAddress.getByName(args[1]);
-        short prefix = Short.parseShort(args[2]);
-        ports.add(p);
-        NetworkNumber network = new NetworkNumber(address, prefix);
-        LocalSubnet direct = LocalSubnet.create(network, address, p, protocol);
-        RoutingTable.getInstance().add(direct);
-        rip.addStaticRoute(direct);
-
-        p = new EthernetIpSupport(new EthernetPort(args[3]));
-        address = InetAddress.getByName(args[4]);
-        prefix = Short.parseShort(args[5]);
-        ports.add(p);
-        network = new NetworkNumber(address, prefix);
-        direct = LocalSubnet.create(network, address, p, protocol);
-        RoutingTable.getInstance().add(direct);
-        rip.addStaticRoute(direct);
-
-        // skip routing if multicast
-        IpMulticastHandler multicast = new IpMulticastHandler(protocol, route);
-        
-        rip.start();
-        
-        for (EthernetIpSupport port : ports) {
-            ArpHandler arp = new ArpHandler();
-            port.getPort().register(Ethertype.ARP, arp);
-            port.register(multicast);
-        }
-
-        for (EthernetIpSupport ep : ports) {
-            ep.getPort().start();
-        }
-        for (EthernetIpSupport ep : ports) {
-            try {
-                ep.getPort().join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        */
 
         for (int i = 0; i < 2; i++) {
         	eths[i].start();
